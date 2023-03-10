3a21d40bfce0ecb8cdcff29432ae7bc5e24eda7f
susanee <comtrial97@gmail.com>
2023-03-10 17:11:32 +0900

    feat:orderService

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,28 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+    MemberRepository memberRepository = new MemoryMemberRepository();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        //given
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberRepository.save(member);
+
+        //when
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
